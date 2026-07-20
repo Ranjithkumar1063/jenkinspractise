c0c25e1c0dca35c89948784dfb6bba731695a004
Ranjithkumar <Ranjithkumar1063sky@gmail.com>
2026-07-20 16:09:06 +0530

    loginadded

---
+++ tests/login.spec.js
@@ -0,0 +1,17 @@
+
+import {test,expect} from '@playwright/test';
+
+test('Login @smoke', async ({ page }) => {
+
+ await page.goto("http://49.249.29.4:8081/TestServer/Build/Health_Care_Hospital_System/")
+ 
+
+ await page.getByPlaceholder('Username').click();
+ await page.getByPlaceholder('Username').fill('superadmin');
+ await page.getByPlaceholder('Password').click()
+await page.getByPlaceholder('Password').fill('123');
+await page.locator("//button[text()='SUBMIT']").click()
+
+}
+    
+)
